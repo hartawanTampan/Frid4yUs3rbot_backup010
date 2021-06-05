15cda10c963ca10287b3f9ce0988ac72f31228f1
スタークギャング <StarkGangZ@gmail.com>
2021-06-05 22:01:37 +0530

    Update xtra_plugins.sh

--- bot_utils_files/other_helpers/xtra_plugins.sh
+++ bot_utils_files/other_helpers/xtra_plugins.sh
@@ -6,20 +6,31 @@
 #
 # All rights reserved.
 
-PLUGIN_REPO="$1"
-BRANCH="$2"
+PLUGIN_REPO="https://github.com/DevsExpo/Xtra-Plugins"
+BRANCH="unstable"
 xtra_fold="./xtraplugins"
 req_file="./xtraplugins/req.txt"
+
+make_xtra_dir () {
+  if [[ -d "$xtra_fold" ]] 
+  then
+    rm -r "$xtra_fold"
+    mkdir "$xtra_fold"
+  else
+    mkdir "$xtra_fold"
+  fi
+}
 
 git_clone_plugin_repo () {
   git clone "$PLUGIN_REPO" -b "$BRANCH" "$xtra_fold"
 }
 
 xtra_pip_installer () {
-    pip3 install --no-cache-dir -r "$req_file"
+    pip3 install -r "$req_file"
 }
 
 fetch_xtra_plugins () {
+  make_xtra_dir
   git_clone_plugin_repo
   xtra_pip_installer
 }
