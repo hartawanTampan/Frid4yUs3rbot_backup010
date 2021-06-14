e03ed34a1ab887375c4f4dc2705c03e5f24d0350
スタークギャング <StarkGangZ@gmail.com>
2021-06-14 12:22:35 +0530

    Update paste.py

--- plugins/paste.py
+++ plugins/paste.py
@@ -14,6 +14,7 @@ from main_startup.core.decorators import friday_on_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 
 
+
 @friday_on_cmd(
     ["paste"],
     cmd_help={
