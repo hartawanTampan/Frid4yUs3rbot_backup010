62a2934970c47cd9348a8d4f217811dd7caf14d2
スタークギャング <StarkGangZ@gmail.com>
2021-06-20 20:19:16 +0530

    Update paste.py

--- plugins/paste.py
+++ plugins/paste.py
@@ -7,9 +7,7 @@
 # All rights reserved.
 
 import os
-
-import requests
-
+import aiohttp
 from main_startup.core.decorators import friday_on_cmd
 from main_startup.helper_func.basic_helpers import edit_or_reply, get_text
 
